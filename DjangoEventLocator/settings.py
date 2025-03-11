2da96451186d456e7a7c5d5300706ecf97830b04
SabirWalid <s.abdurahma@alustudent.com>
2025-03-11 17:48:46 +0200

    event

--- DjangoEventLocator/settings.py
+++ DjangoEventLocator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-osco2s-($#t&g0(a(tp)p3byrgo-@*a0mkxrw27+a75h564za5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
